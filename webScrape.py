4fdb45d58be3afa1a96b178400efef530e2dc9c2
Sean L McFall <sean.l.mcfall@gmail.com>
2015-06-18 13:58:14 -0600

    one river output to a csv

--- webScrape.py
+++ webScrape.py
@@ -4,6 +4,7 @@ __author__ = 'smcfall'
 
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 
 url = "www.wdfw.wa.gov/fishing/creel/steelhead/"
@@ -17,18 +18,29 @@ soup = BeautifulSoup(data)
 # get table title
 
 # get table cells
-table1 = soup(bgcolor="#666666")[0]
+table1 = soup(bgcolor="#666666")[0]i9
 
 count = 0
 totals = len(table1) - 2
+riverName = "Bogachiel/Quillayute River"
 
-for i in table1:
-    # drop empty elements
-    if len(i) > 1 and count > 3 and count < totals:
-        date = i.contents[1].string
-        anglers = i.contents[3].string
-        wsKept = i.contents[5].string
+with open('boga.csv', 'wb') as csvfile:
+    boga = csv.writer(csvfile, delimiter=',')
+    boga.writerow(["River Name", "Date", "Num of Anglers", "WS Kept", "WS Rel",
+                   "HS Kept", "HS Rel", "Total Hours Fished", "Comments"])
 
-        print "{} {} {}".format(str(date), int(anglers), int(wsKept))
+    for i in table1:
+        # drop empty elements
+        if len(i) > 1 and count > 3 and count < totals:
+            date = str(i.contents[1].string)
+            anglers = int(i.contents[3].string)
+            wsKept = int(i.contents[5].string)
+            wsRel = int(i.contents[7].string)
+            hKept = int(i.contents[9].string)
+            hRel = int(i.contents[11].string)
+            hrs = float(i.contents[13].string)
+            comments = str(i.contents[15].string)
 
-    count += 1
+            boga.writerow([riverName,date,anglers,wsKept,wsRel,hKept,hRel,hrs,comments])
+        count += 1
+
