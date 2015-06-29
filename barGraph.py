75d8f4076bc5d65857979377d73623f9088eae09
slmcfall <sean.l.mcfall@gmail.com>
2015-06-29 16:50:10 -0600

    failure to create a well-fitted line

--- barGraph.py
+++ barGraph.py
@@ -1,6 +1,8 @@
 # bar graphs for washington fisheries data
 
 import numpy as np
+from numpy.polynomial import Chebyshev as T
+
 import csv
 
 import matplotlib.pyplot as plt
@@ -44,16 +46,19 @@ empty_list = ['']*len(date)
 
 # set size of the figure area
 fig = plt.figure(figsize=(15, 10))
-fig.suptitle('Calawah River 2014/2015', fontsize=14, fontweight='bold')
+title = 'Calawah River 2014/2015'
+title.upper()
+fig.suptitle(title.upper(), fontsize=16, fontweight='bold')
 # position, attributes, title
 
-def makeBar(position, data, x_labels, y_title, line_bf):
+def makeBar(position, data, x_labels, y_title, fill_color, line_bf):
     ax = fig.add_subplot(position)
-    ax.bar(x_axis, data, width, color=bar_color, align='center')
+    ax.bar(x_axis, data, width, color=fill_color, align='center')
     labels = plt.xticks(x_axis, x_labels, rotation='vertical', ha='center')
     plt.grid(True)
-    ax.set_ylabel(y_title)
+    ax.set_ylabel(y_title, fontweight='bold')
     plt.xlim([0,len(x_axis)+1])
+
 
     if line_bf == 1:
 
@@ -65,33 +70,28 @@ def makeBar(position, data, x_labels, y_title, line_bf):
 
         # add a 'best fit' line
         deg = round(max(x_axis)/3)
-        # y = np.polyfit(x_axis, data, deg)
-        # l = plt.plot(x_axis, y, 'r--', linewidth=2)
 
-        coefficients = np.polyfit(x_axis, data, deg+1)
+        if deg % 2 == 0:
+            deg += 1
+
+        coefficients = np.polyfit(x_axis, data, deg)
+        print coefficients
         polynomial = np.poly1d(coefficients)
         xs = np.linspace(min(data), max(data), 1000)
         ys = polynomial(xs)
+
         plt.plot(xs,ys)
-
-        # x_sm = np.array(x_axis)
-        # y_sm = np.array(data)
-
-        # x_smooth = np.linspace(x_sm.min(), x_sm.max(), 200)
-        # y_smooth = spline(x_sm, y_sm, x_smooth)
-
-        # plt.plot(x_smooth, y_smooth, '#FF5252', linewidth=1)
     
     axes = plt.gca()
     axes.set_ylim(min(data),max(data))
 
 # six plots
-num_ang_ax = makeBar(321,num_ang, empty_list,'Number of Anglers',1)
-hrs_fished_ax = makeBar(322,hrs_fished,empty_list,'Hours Fished',1)
-ws_kept_ax = makeBar(323,ws_kept,empty_list,'Wild Steelhead Kept',0)
-ws_rel_ax = makeBar(324,ws_rel,empty_list,'Wild Steelhead Released',0)
-h_kept_ax = makeBar(325,h_kept,date,'Hatchery Steelhead Kept',0)
-h_rel_ax = makeBar(326,h_rel,date,'Hatchery Steelhead Released',0)
+num_ang_ax = makeBar(321,num_ang, empty_list,'Number of Anglers',bar_color,1)
+hrs_fished_ax = makeBar(322,hrs_fished,empty_list,'Hours Fished',bar_color,1)
+ws_kept_ax = makeBar(323,ws_kept,empty_list,'Wild Steelhead Kept', '#FF5252',0)
+ws_rel_ax = makeBar(324,ws_rel,empty_list,'Wild Steelhead Released', '#FF5252',1)
+h_kept_ax = makeBar(325,h_kept,date,'Hatchery Steelhead Kept','#8bc34a',1)
+h_rel_ax = makeBar(326,h_rel,date,'Hatchery Steelhead Released','#8bc34a',1)
 
 # gives the x-axis labels enough room
 fig.tight_layout()
