31b4d7d2e7e77563a8cf355bec425bcfe8c7befc
slmcfall <sean.l.mcfall@gmail.com>
2015-06-30 14:03:42 -0600

    iteratively creates figures for all rivers now, though not elegantly...im using global variables...

--- barGraph.py
+++ barGraph.py
@@ -1,15 +1,9 @@
 # bar graphs for washington fisheries data
 
 import numpy as np
-from numpy.polynomial import Chebyshev as T
-
 import csv
-
+import os
 import matplotlib.pyplot as plt
-import matplotlib.mlab as mlab
-
-from scipy.interpolate import spline
-from scipy.stats import norm
 
 #table_path = "C:\\Users\\Sean.McFall\\PycharmProjects\\fisheries-webscrape\\tables\\"
 #fig_path = "C:\\Users\\Sean.McFall\\PycharmProjects\\fisheries-webscrape\\figures\\"
@@ -17,44 +11,7 @@ from scipy.stats import norm
 table_path = "/home/smcfall/Documents/fisheries-webscrape/tables/"
 fig_path = "/home/smcfall/Documents/fisheries-webscrape/figures/"
 
-# read the csv
-res = csv.reader(open(table_path + "Cal.csv"), delimiter = ',')
-# skips the header
-res.next()
-
-# ang = anglers, ws = wild steelhead, h = hatchery steelhed, rel = released
-date, num_ang, hrsPerWS, wsCaught, hrsPerHS, hsCaught, hrs_fished = [], [], [], [], [], [], []
-
-# populate lists from csv
-for col in res:
-    date.append(col[1])
-    num_ang.append(int(col[2]))
-    hrsPerWS.append(float(col[3]))
-    wsCaught.append(int(col[4]))
-    hrsPerHS.append(float(col[5]))
-    hsCaught.append(int(col[6]))
-    hrs_fished.append(float(col[11]))
-
-#
-# bar plot creation
-#
-
-width = 0.8
-x_axis = range(1,len(date)+1)
-bar_color = '#455A64'  # slate
-empty_list = ['']*len(date)
-
-# set size of the figure area
-fig = plt.figure(figsize=(15, 10))
-title = 'Calawah River 2014/2015'
-title.upper()
-fig.suptitle(title.upper(), fontsize=16, fontweight='bold')
-# position, attributes, title
-
-def fit_poly_through_origin(x, y, n=1):
-    a = x[:, np.newaxis] ** np.arange(1, n+1)
-    coeff = np.linalg.lstsq(a, y)[0]
-    return np.concatenate(([0], coeff))
+tables = os.listdir(table_path)
 
 def makeBar(position, data, x_labels, y_title, fill_color, line_bf):
     ax = fig.add_subplot(position)
@@ -89,16 +46,67 @@ def makeBar(position, data, x_labels, y_title, fill_color, line_bf):
     axes = plt.gca()
     axes.set_ylim(min(data),max(data))
 
-# six plots
-num_ang_ax = makeBar(322,num_ang, empty_list,'Number of Anglers',bar_color,1)
-hrs_fished_ax = makeBar(321,hrs_fished,empty_list,'Hours Fished',bar_color,1)
-hrsPerWS_ax = makeBar(323,hrsPerWS,empty_list,'Hours Per \n Wild Steelhead','#FF5252',0)
-ws_caught_ax = makeBar(324,wsCaught,empty_list,'Wild Steelhead Caught','#FF5252',0)
-hrs_per_ws_ax = makeBar(325,hrsPerHS,date,'Hours Per \n Hatchery Steelhead','#8bc34a',0)
-hs_caught_ax = makeBar(326,hsCaught,date,'Hatchery Steelhead Caught','#8bc34a',0)
+def createFig(table_name):
+    global fig 
+    global x_axis
+    global width
+    #open csv
 
-# gives the x-axis labels enough room
-fig.tight_layout()
-plt.subplots_adjust(top=.92)
-#plt.savefig(fig_path + 'test.png', bbox_inches="tight")
-plt.show()
+    # get list of csv files
+    # iterate over list of files
+
+    # read the csv
+    res = csv.reader(open(table_path + table_name), delimiter = ',')
+
+    # ang = anglers, ws = wild steelhead, h = hatchery steelhed, rel = released
+    date, num_ang, hrsPerWS, wsCaught, hrsPerHS, hsCaught, hrs_fished = [], [], [], [], [], [], []
+
+    # populate lists from csv
+    for col in res:
+        date.append(col[1])
+        num_ang.append(int(col[2]))
+        hrsPerWS.append(float(col[3]))
+        wsCaught.append(int(col[4]))
+        hrsPerHS.append(float(col[5]))
+        hsCaught.append(int(col[6]))
+        hrs_fished.append(float(col[11]))
+
+        river_name = col[0]
+
+    #
+    # bar plot creation
+    #
+
+    width = 0.8
+    x_axis = range(1,len(date)+1)
+    bar_color = '#455A64'  # slate
+    empty_list = ['']*len(date)
+
+    # set size of the figure area
+    
+    fig = plt.figure(figsize=(15, 10))
+    title = river_name + '2014/2015'
+    title.upper()
+    fig.suptitle(title.upper(), fontsize=16, fontweight='bold')
+    # position, attributes, title
+
+    # six plots
+    num_ang_ax = makeBar(322,num_ang, empty_list,'Number of Anglers',bar_color,0)
+    hrs_fished_ax = makeBar(321,hrs_fished,empty_list,'Hours Fished',bar_color,0)
+    hrsPerWS_ax = makeBar(323,hrsPerWS,empty_list,'Hours Per \n Wild Steelhead','#FF5252',0)
+    ws_caught_ax = makeBar(324,wsCaught,empty_list,'Wild Steelhead Caught','#FF5252',0)
+    hrs_per_ws_ax = makeBar(325,hrsPerHS,date,'Hours Per \n Hatchery Steelhead','#8bc34a',0)
+    hs_caught_ax = makeBar(326,hsCaught,date,'Hatchery Steelhead Caught','#8bc34a',0)
+
+    # gives the x-axis labels enough room
+    fig.tight_layout()
+    plt.subplots_adjust(top=.92)
+    #plt.savefig(fig_path + 'test.png', bbox_inches="tight")
+    #plt.show()
+
+    # save the figure
+    plt.savefig(fig_path + table_name[:3] + 'png', bbox_inches='tight')
+
+for table in tables:
+    if table[-3:] == 'csv':
+        createFig(table)
